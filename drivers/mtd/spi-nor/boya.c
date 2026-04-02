bd6c220bc3d81fe0f5f5bc75f296469878d49e78
evtest-hash <84323112+evtest-hash@users.noreply.github.com>
2026-04-02 17:44:37 +0800

    spi-nor: boya: add support for BY25D40ESTIG

--- drivers/mtd/spi-nor/boya.c
+++ drivers/mtd/spi-nor/boya.c
@@ -13,6 +13,8 @@ static const struct flash_info boya_parts[] = {
 		FIXUP_FLAGS(SPI_NOR_4B_OPCODES) },
 	{ "BY25Q64ESSIG", INFO(0x684017, 0, 64 * 1024, 128)
 		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	{ "BY25D40ESTIG", INFO(0x684013, 0, 64 * 1024, 8)
+		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ) },
 };
 
 const struct spi_nor_manufacturer spi_nor_boya = {
